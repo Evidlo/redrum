0bdb266c7469e0edab79f61a58678a40212d19db
evan <evan@evanw.org>
2016-12-26 03:59:12 -0500

    remove seen filter

--- tune.py
+++ tune.py
@@ -11,23 +11,26 @@
 from imgurt import *
 
 # put some images of varying quality here to see their score before and after logistic function
-links = ['http://i.imgur.com/D331RXf.jpg', # an image with bad aspect ratio, resolution
-         'http://i.imgur.com/P7I7bML.jpg'] # good aspect ratio, resolution
+# these images must be from imgurt_cache
+links = ['D331RXf', # bad aspect ratio and resolution, high views
+         'P7I7bML', # good aspect ratio and resolution
+         'GW2i3K4', # good ratio and resolution, low views
+         'QlyUhRY',
+         'J9zLI00',
+         'WXeaCbp',
+         ]
 
 
 f = open(cache_file, 'r')
 j = json.loads(f.read())
 images = j['images']
-seen = j['seen']
-if unseen_only:
-    images = [image for image in images if image['id'] not in seen]
 
 max_views = max([image['views'] for image in images])
 
 print("%-12s%-12s%-12s%-7s  |  %-12s%-12s%-7s  |  %-12s" % ("ID", "ratio", "views", "pixel", "ratio_l", "views_l", "pixel_l", "final"))
 print("===============================================================================================")
 for link in links:
-    image = [image for image in images if image['link'] == link][0]
+    image = [image for image in images if image['id'] == link][0]
 
     [final_score,
      ratio_score,
