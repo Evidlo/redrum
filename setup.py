0a3349b3d5871790861238894b2d03a75139b333
evan <evan@evanw.org>
2017-03-31 19:22:14 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
 from distutils.core import setup
-setup(name='imgurt'
-      version='1.0'
-      py_modules=['requests'])
+
+setup(
+    name='imgurt',
+    version='1.1',
+    packages=['imgurt'],
+    author="Evan Widloski",
+    author_email="evan@evanw.org",
+    description="uses math to select wallpapers from Reddit"
+    license="MIT",
+    keywords="imgur reddit wallpaper changer",
+    url="https://github.com/evidlo/imgurt",
+    install_requires=[
+        "PyYAML",
+        "requests",
+        "json",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
